e4c5b9bcf669748fa845583e9aef4dbbe6cb8f8e
Mehedi Hasan <mehedi.hasan@appscode.com>
2022-12-12 14:58:39 +0600

    clean
    Signed-off-by: Mehedi Hasan <mehedi.hasan@appscode.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -64,7 +64,6 @@ delete_cluster_metadata() {
 AUTHFILE="/opt/kafka/config/kafka_server_jaas.conf"
 sed -i "s/\<KAFKA_USER\>/"$KAFKA_USER"/g" $AUTHFILE
 sed -i "s/\<KAFKA_PASSWORD\>/"$KAFKA_PASSWORD"/g" $AUTHFILE
-cat $AUTHFILE
 #export KAFKA_OPTS="-Djava.security.auth.login.config=$AUTHFILE"
 export KAFKA_OPTS="$KAFKA_OPTS -Djava.security.auth.login.config=$AUTHFILE"
 
