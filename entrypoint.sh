10e23424c4b4bc2a271937918ed8ad339c9aef28
Tamal Saha <tamal@appscode.com>
2023-03-02 16:04:30 -0800

    Format script (#2)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,55 +11,51 @@ server_config="/opt/kafka/config/kraft/server.properties"
 
 cp $temp_operator_config $operator_config
 if [[ -f $temp_ssl_config ]]; then
-  cat $temp_ssl_config $operator_config > config.properties.updated
-  mv config.properties.updated $operator_config
-  cp $temp_ssl_config /opt/kafka/config
+    cat $temp_ssl_config $operator_config >config.properties.updated
+    mv config.properties.updated $operator_config
+    cp $temp_ssl_config /opt/kafka/config
 fi
 
-
-
 if [[ -f $temp_clientauth_config ]]; then
-  cp $temp_clientauth_config /opt/kafka/config
+    cp $temp_clientauth_config /opt/kafka/config
 fi
 
 if [[ $KAFKA_PASSWORD != "" ]]; then
-  CLIENTAUTHFILE="/opt/kafka/config/clientauth.properties"
-  sed -i "s/\<KAFKA_USER\>/"$KAFKA_USER"/g" $CLIENTAUTHFILE
-  sed -i "s/\<KAFKA_PASSWORD\>/"$KAFKA_PASSWORD"/g" $CLIENTAUTHFILE
+    CLIENTAUTHFILE="/opt/kafka/config/clientauth.properties"
+    sed -i "s/\<KAFKA_USER\>/"$KAFKA_USER"/g" $CLIENTAUTHFILE
+    sed -i "s/\<KAFKA_PASSWORD\>/"$KAFKA_PASSWORD"/g" $CLIENTAUTHFILE
 fi
 
-
-while IFS='=' read -r key value
-do
+while IFS='=' read -r key value; do
     key=$(echo $key | tr '.' '_')
     eval ${key}=\${value}
-done < "$operator_config"
+done <"$operator_config"
 
 CLUSTER_ID=${cluster_id}
 ID=${HOSTNAME##*-}
-NODE=$(echo $HOSTNAME | rev | cut -d- -f1 --complement | rev )
+NODE=$(echo $HOSTNAME | rev | cut -d- -f1 --complement | rev)
 CONTROLLER_NODE_COUNT=${controller_count}
 
 delete_cluster_metadata() {
-  NODE_ID=$1
-  echo "Enter for metadata deleting node $NODE_ID"
-  if [[ ! -d "$log_dirs/$NODE_ID" ]]; then
-    mkdir -p "$log_dirs"/"$NODE_ID"
-    echo "Created kafka data directory at "$log_dirs"/$NODE_ID"
+    NODE_ID=$1
+    echo "Enter for metadata deleting node $NODE_ID"
+    if [[ ! -d "$log_dirs/$NODE_ID" ]]; then
+        mkdir -p "$log_dirs"/"$NODE_ID"
+        echo "Created kafka data directory at "$log_dirs"/$NODE_ID"
 
-  else
-    echo "Deleting old metadata..."
-    rm -rf $log_dirs/$NODE_ID/meta.properties
-    if [[ -d "$metadata_log_dir/__cluster_metadata-0" ]]; then
-       rm -rf $metadata_log_dir/meta.properties
+    else
+        echo "Deleting old metadata..."
+        rm -rf $log_dirs/$NODE_ID/meta.properties
+        if [[ -d "$metadata_log_dir/__cluster_metadata-0" ]]; then
+            rm -rf $metadata_log_dir/meta.properties
+        fi
     fi
-  fi
 
-  if [[ ! -f "$log_dirs/cluster_id" ]]; then
-      echo "$CLUSTER_ID" > "$log_dirs"/cluster_id
-  else
-      CLUSTER_ID=$(cat "$log_dirs"/cluster_id)
-  fi
+    if [[ ! -f "$log_dirs/cluster_id" ]]; then
+        echo "$CLUSTER_ID" >"$log_dirs"/cluster_id
+    else
+        CLUSTER_ID=$(cat "$log_dirs"/cluster_id)
+    fi
 
 }
 
@@ -70,70 +66,71 @@ export KAFKA_OPTS="$KAFKA_OPTS -Djava.security.auth.login.config=$AUTHFILE"
 
 if [[ $process_roles = "controller" ]]; then
 
-  delete_cluster_metadata $ID
+    delete_cluster_metadata $ID
 
-  echo "node.id=$ID" >> /opt/kafka/config/kafkaconfig/config.properties
+    echo "node.id=$ID" >>/opt/kafka/config/kafkaconfig/config.properties
 
-  sed -e "s+^log.dirs=.*+log.dirs=$log_dirs/$ID+" \
-  /opt/kafka/config/kafkaconfig/config.properties > config.properties.updated
-  mv config.properties.updated /opt/kafka/config/kafkaconfig/config.properties
+    sed -e "s+^log.dirs=.*+log.dirs=$log_dirs/$ID+" \
+        /opt/kafka/config/kafkaconfig/config.properties >config.properties.updated
+    mv config.properties.updated /opt/kafka/config/kafkaconfig/config.properties
 
-  cat /opt/kafka/config/kafkaconfig/config.properties /opt/kafka/config/kraft/controller.properties | awk -F= '!seen[$1]++' > controller.properties.updated
-  mv controller.properties.updated /opt/kafka/config/kraft/controller.properties
+    cat /opt/kafka/config/kafkaconfig/config.properties /opt/kafka/config/kraft/controller.properties | awk -F= '!seen[$1]++' >controller.properties.updated
+    mv controller.properties.updated /opt/kafka/config/kraft/controller.properties
 
-  if [[ -f "$ssl_config" ]]; then
-      cat $ssl_config $controller_config | awk -F'=' '!seen[$1]++' > controller.properties.updated
-      mv controller.properties.updated $controller_config
-  fi
+    if [[ -f "$ssl_config" ]]; then
+        cat $ssl_config $controller_config | awk -F'=' '!seen[$1]++' >controller.properties.updated
+        mv controller.properties.updated $controller_config
+    fi
 
-  kafka-storage.sh format -t "$CLUSTER_ID" -c /opt/kafka/config/kraft/controller.properties --ignore-formatted
+    kafka-storage.sh format -t "$CLUSTER_ID" -c /opt/kafka/config/kraft/controller.properties --ignore-formatted
 
-  echo "Starting Kafka Server"
-  exec kafka-server-start.sh /opt/kafka/config/kraft/controller.properties
+    echo "Starting Kafka Server"
+    exec kafka-server-start.sh /opt/kafka/config/kraft/controller.properties
 
 elif [[ $process_roles = "broker" ]]; then
-  ID=$(( ID + CONTROLLER_NODE_COUNT ))
-  delete_cluster_metadata $ID
+    ID=$((ID + CONTROLLER_NODE_COUNT))
+    delete_cluster_metadata $ID
 
-  echo "node.id=$ID" >> $operator_config
+    echo "node.id=$ID" >>$operator_config
 
-  sed -e "s+^log.dirs=.*+log.dirs=$log_dirs/$ID+" \
-  $operator_config > $operator_config.updated
-  mv $operator_config.updated $operator_config
+    sed -e "s+^log.dirs=.*+log.dirs=$log_dirs/$ID+" \
+        $operator_config >$operator_config.updated
+    mv $operator_config.updated $operator_config
 
-  cat $operator_config $broker_config | awk -F'=' '!seen[$1]++' > $broker_config.updated
-  mv $broker_config.updated $broker_config
+    cat $operator_config $broker_config | awk -F'=' '!seen[$1]++' >$broker_config.updated
+    mv $broker_config.updated $broker_config
 
-  if [[ -f "$ssl_config" ]]; then
-      cat $ssl_config $broker_config | awk -F'=' '!seen[$1]++' > $broker_config.updated
-      mv $broker_config.updated $broker_config
-  fi
+    if [[ -f "$ssl_config" ]]; then
+        cat $ssl_config $broker_config | awk -F'=' '!seen[$1]++' >$broker_config.updated
+        mv $broker_config.updated $broker_config
+    fi
 
-  echo "Formatting broker properties"
+    echo "Formatting broker properties"
 
-  kafka-storage.sh format -t $CLUSTER_ID -c $broker_config --ignore-formatted
+    kafka-storage.sh format -t $CLUSTER_ID -c $broker_config --ignore-formatted
 
-  echo "Starting Kafka Server"
-  exec kafka-server-start.sh $broker_config
+    echo "Starting Kafka Server"
+    exec kafka-server-start.sh $broker_config
 
-else [[ $process_roles = "controller,broker" ]]
+else
+    [[ $process_roles = "controller,broker" ]]
 
-  delete_cluster_metadata $ID
+    delete_cluster_metadata $ID
 
-  echo "node.id=$ID" >> $operator_config
-  sed -e "s+^log.dirs=.*+log.dirs=$log_dirs/$ID+" \
-  $operator_config > $operator_config.updated
-  mv $operator_config.updated $operator_config
+    echo "node.id=$ID" >>$operator_config
+    sed -e "s+^log.dirs=.*+log.dirs=$log_dirs/$ID+" \
+        $operator_config >$operator_config.updated
+    mv $operator_config.updated $operator_config
 
-  cat $operator_config $server_config | awk -F'=' '!seen[$1]++' > $server_config.updated
-  mv $server_config.updated $server_config
+    cat $operator_config $server_config | awk -F'=' '!seen[$1]++' >$server_config.updated
+    mv $server_config.updated $server_config
 
-  if [[ -f "$ssl_config" ]]; then
-      cat $ssl_config $server_config | awk -F'=' '!seen[$1]++' > $server_config.updated
-      mv $server_config.updated $server_config
-  fi
+    if [[ -f "$ssl_config" ]]; then
+        cat $ssl_config $server_config | awk -F'=' '!seen[$1]++' >$server_config.updated
+        mv $server_config.updated $server_config
+    fi
 
-  kafka-storage.sh format -t $CLUSTER_ID -c $server_config --ignore-formatted
-  echo "Starting Kafka Server"
-  exec kafka-server-start.sh $server_config
+    kafka-storage.sh format -t $CLUSTER_ID -c $server_config --ignore-formatted
+    echo "Starting Kafka Server"
+    exec kafka-server-start.sh $server_config
 fi
