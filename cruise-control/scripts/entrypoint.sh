edcaf11d7e1cd6556fc330f572138894f32697c2
raihankhan <raihan@appscode.com>
2023-08-13 20:34:49 +0600

    Remove jaas config
    Signed-off-by: raihankhan <raihan@appscode.com>

--- cruise-control/scripts/entrypoint.sh
+++ cruise-control/scripts/entrypoint.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
-cc_config_dir="/opt/cruise-control/config"
-cc_config="/opt/cruise-control/config/cruisecontrol.properties"
-cc_temp_config="/opt/cruise-control/temp-config/cruisecontrol.properties"
-capacity_config="/opt/cruise-control/config/capacity.json"
-capacity_temp_config="/opt/cruise-control/temp-config/capacity.json"
-cc_ui_config="/opt/cruise-control/cruise-control-ui/dist/static/config.csv"
-cc_ui_temp_config="/opt/cruise-control/temp-config/config.csv"
-brokerSet_config="/opt/cruise-control/config/brokerSets.json"
-brokerSet_temp_config="/opt/cruise-control/temp-config/brokerSets.json"
+set -eo pipefail
+set -x
+
+cc_default_config_dir=/opt/cruise-control/config
+cc_ui_default_config_dir=/opt/cruise-control/cruise-control-ui/dist/static
+cc_temp_config_dir=/opt/cruise-control/temp-config
+cc_custom_config_dir=/opt/cruise-control/custom-config
+
+## cruise control default configs
+cc_config=$cc_default_config_dir/cruisecontrol.properties
+capacity_config=$cc_default_config_dir/capacity.json
+cc_ui_config=$cc_ui_default_config_dir/config.csv
+brokerSet_config=$cc_default_config_dir/brokerSets.json
+cluster_config=$cc_default_config_dir/clusterConfigs.json
+
+## cruise control operator generated configs
+cc_temp_config=$cc_temp_config_dir/cruisecontrol.properties
+capacity_temp_config=$cc_temp_config_dir/capacity.json
+cc_ui_temp_config=$cc_temp_config_dir/config.csv
+brokerSet_temp_config=$cc_temp_config_dir/brokerSets.json
+
+## cruise control custom configs
+cc_custom_config=$cc_custom_config_dir/cruisecontrol.properties
+cc_custom_capacity_config=$cc_custom_config_dir/capacity.json
+cc_custom_ui_config=$cc_custom_config_dir/config.csv
+cc_custom_brokerSet_config=$cc_custom_config_dir/brokerSets.json
+cc_custom_clusterConfig=$cc_custom_config_dir/clusterConfigs.json
 
 
-# merge the operator generated cruise control config with the default one
+## merge the operator generated cruise control config with the default one
 if [[ -f $cc_temp_config ]]; then
-  $cc_config_dir/merge_custom_config.sh $cc_temp_config $cc_config $cc_config_dir/cruisecontrol.properties.updated
-  cp $cc_config_dir/cruisecontrol.properties.updated $cc_config
+  $cc_default_config_dir/merge_custom_config.sh $cc_temp_config $cc_config $cc_default_config_dir/cruisecontrol.properties.updated
+  cp $cc_default_config_dir/cruisecontrol.properties.updated $cc_config
 fi
 
 # Replace the default broker capacity config with operator generated one
@@ -32,11 +50,35 @@ if [[ -f $cc_ui_temp_config ]]; then
   cp $cc_ui_temp_config $cc_ui_config
 fi
 
-rm $cc_config_dir/clusterConfigs.json
+# Remove the default Cluster config json as operator is not generating one
+if [[ -f $cluster_config ]]; then
+  rm $cluster_config
+fi
 
+## merge/replace the operator generated cruise control config with the user provided one
+if [[ -f $cc_custom_config ]]; then
+  $cc_default_config_dir/merge_custom_config.sh $cc_custom_config $cc_config $cc_default_config_dir/cruisecontrol.properties.updated
+  cp $cc_default_config_dir/cruisecontrol.properties.updated $cc_config
+fi
 
-if [[ $KAFKA_PASSWORD != "" && $KAFKA_USER != "" ]]; then
-   echo "sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username='${KAFKA_USER}' password='${KAFKA_PASSWORD}';" >> $cc_config
+# Replace the default broker capacity config with operator generated one
+if [[ -f $cc_custom_capacity_config ]]; then
+  cp $cc_custom_capacity_config $capacity_config
+fi
+
+# Replace the default BrokerSets config with operator generated one
+if [[ -f $cc_custom_brokerSet_config ]]; then
+  cp $cc_custom_brokerSet_config $brokerSet_config
+fi
+
+# Replace the default CC UI config csv with operator generated one
+if [[ -f $cc_custom_ui_config ]]; then
+  cp $cc_custom_ui_config $cc_ui_config
+fi
+
+# Remove the default Cluster config json as operator is not generating one
+if [[ -f $cc_custom_clusterConfig ]]; then
+  cp $cc_custom_clusterConfig $cluster_config
 fi
 
 ./kafka-cruise-control-start.sh $cc_config 9090
