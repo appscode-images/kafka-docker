785938ef9c7905043ae34e251388221d76ac0ac9
M Obaydullah <52800314+obaydullahmhs@users.noreply.github.com>
2023-11-24 17:49:09 +0600

    Reconfigure script for broker properties changes and update base image to use Java 17 (#17)
    Co-authored-by: raihankhan <raihan@appscode.com>

--- kafka/scripts/merge_custom_config.sh
+++ kafka/scripts/merge_custom_config.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
 master_file=$1
 slave_file=$2
 output_file=$3
+
 #ignore the following properties while merging
-arr=("process.roles" "cluster.id" "node.id" "controller.quorum.voters"
-"control.plane.listener.name" "listeners" "advertised.listeners")
+forbidden_configs=("process.roles" "cluster.id" "node.id" "controller.quorum.voters"
+"control.plane.listener.name")
+
 # Ensure files exist before attempting to merge
 if [ ! -e $master_file ] ; then
     exit
@@ -13,22 +16,26 @@ if [ ! -e $slave_file ] ; then
     echo 'Unable to merge custom configuration property files: $slave_file doesn''t exist'
     exit
 fi
+
 # Delete the previous output file if exists
 if [ -e $output_file ] ; then
     rm -rf "$output_file"
 fi
+
 echo "Merging custom config with default config"
 # Read property files into arrays
 readarray master_file_a < "$master_file"
 readarray slave_file_a < "$slave_file"
+
 # Regex strings to check for values and
 COMMENT_LINE_REGEX="[#*]"
 HAS_VALUE_REGEX="[*=*]"
 declare -A all_properties
+
 # All the master file property names and values will be preserved
 for master_file_line in "${master_file_a[@]}"; do
     master_property_name=`echo $master_file_line | cut -d = -f1`
-    if [[ $(echo ${arr[@]} | fgrep -w $master_property_name) ]]; then
+    if [[ $(echo ${forbidden_configs[@]} | fgrep -w $master_property_name) ]]; then
         continue
     else
         # Only attempt to get the property value if it exists
@@ -43,6 +50,7 @@ for master_file_line in "${master_file_a[@]}"; do
         fi
     fi
 done
+
 # Properties that are in the slave but not the master will be preserved
 for slave_file_line in "${slave_file_a[@]}"; do
     slave_property_name=`echo $slave_file_line | cut -d = -f1`
@@ -58,12 +66,18 @@ for slave_file_line in "${slave_file_a[@]}"; do
         if ! [[ $slave_property_name =~ $COMMENT_LINE_REGEX ]]; then
             all_properties[$slave_property_name]=$slave_property_value
         fi
+    else
+        if [[ "$slave_property_name" == "listeners" || "$slave_property_name" == "advertised.listeners" || "$slave_property_name" == "listener.security.protocol.map" ]]; then
+            modified_elements+=("$element")  # Skip modification
+            all_properties[$slave_property_name]="$slave_property_value,${all_properties[$slave_property_name]}"
+        fi
     fi
 done
 
 for key in "${!all_properties[@]}"; do
     echo "$key=${all_properties[$key]}" >> "$output_file"
 done
-# move merge file to kafkaconfig directory
+
+# move merged file to kafkaconfig directory
 mv "$output_file" "$slave_file"
 echo "Merged custom config with default config"
