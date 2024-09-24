f49606a743f82f9c23a95e1a26ff0e6e92f478d4
raihankhan <raihan@appscode.com>
2024-09-24 16:20:01 +0600

    Add scram login for 3.6.0
    Signed-off-by: raihankhan <raihan@appscode.com>

--- kafka/scripts/entrypoint.sh
+++ kafka/scripts/entrypoint.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-kafkaconfig_dir="/opt/kafka/config/kafkaconfig"
+# Final Configuration Path, kafka start will use this configuration
+final_config="/opt/kafka/config/server.properties"
+# KubeDB operator empty directory
+kafka_config_dir="/opt/kafka/config/kafkaconfig"
 operator_config="/opt/kafka/config/kafkaconfig/config.properties"
-ssl_config="/opt/kafka/config/kafkaconfig/ssl.properties"
+# KubeDB operator configuration files
 temp_operator_config="/opt/kafka/config/temp-config/config.properties"
 temp_ssl_config="/opt/kafka/config/temp-config/ssl.properties"
 temp_clientauth_config="/opt/kafka/config/temp-config/clientauth.properties"
+# Kafka KRaft configuration files
 controller_config="/opt/kafka/config/kraft/controller.properties"
 broker_config="/opt/kafka/config/kraft/broker.properties"
 server_config="/opt/kafka/config/kraft/server.properties"
-server_config_file="/opt/kafka/config/custom-config/server.properties"
-broker_config_file="/opt/kafka/config/custom-config/broker.properties"
-controller_config_file="/opt/kafka/config/custom-config/controller.properties"
+# KubeDB Custom configuration files
+server_custom_config="/opt/kafka/config/custom-config/server.properties"
+broker_custom_config="/opt/kafka/config/custom-config/broker.properties"
+controller_custom_config="/opt/kafka/config/custom-config/controller.properties"
+custom_log4j_config="/opt/kafka/config/custom-config/log4j.properties"
+custom_tools_log4j_config="/opt/kafka/config/custom-config/tools-log4j.properties"
+# Utility variables
 kafka_broker_max_id=1000
 
 # For debug purpose
@@ -30,15 +38,21 @@ delete_cluster_metadata() {
 
   NODE_ID=$1
   # Create or update the log directory for specific node
+  modified_log_dirs=()
   echo "Enter for metadata deleting node $NODE_ID"
-  if [[ ! -d "$log_dirs/$NODE_ID" ]]; then
-    mkdir -p "$log_dirs/$NODE_ID"
-    echo "Created kafka data directory at "$log_dirs"/$NODE_ID"
-  else [ -e "$log_dirs/$NODE_ID/meta.properties" ]
-    echo "Deleting old metadata..."
-    rm -rf "$log_dirs/$NODE_ID/meta.properties"
-  fi
-
+  IFS=','
+  for log_dir in $log_dirs; do
+    if [[ ! -d "$log_dir/$NODE_ID" ]]; then
+      mkdir -p "$log_dir/$NODE_ID"
+      echo "Created kafka data directory at $log_dir/$NODE_ID"
+    else [ -e "$log_dir/$NODE_ID/meta.properties" ]
+      echo "Deleting old metadata..."
+      rm -rf "$log_dir/$NODE_ID/meta.properties"
+    fi
+    modified_log_dirs+=("$log_dir/$NODE_ID")
+  done
+  log_dirs=$(IFS=','; echo "${modified_log_dirs[*]}")
+  echo "Modified log_dirs: $log_dirs"
   # Create or update the metadata log directory
   if [[ ! -d "$metadata_log_dir" ]]; then
     mkdir -p $metadata_log_dir
@@ -46,23 +60,20 @@ delete_cluster_metadata() {
   else [ -e "$metadata_log_dir/meta.properties" ]
      rm -rf "$metadata_log_dir/meta.properties"
   fi
-
   # Delete previously configured controller.quorum.voters file
   if [ -e "$metadata_log_dir/__cluster_metadata-0/quorum-state" ] ; then
      rm -rf "$metadata_log_dir/__cluster_metadata-0/quorum-state"
   fi
-  # Add or replace cluster_id to log_dirs/cluster_id
-  echo $CLUSTER_ID > "$log_dirs/cluster_id"
+  # Add or replace cluster_id to metadata_log_dir/cluster_id
+  echo "$KAFKA_CLUSTER_ID" > "$metadata_log_dir/cluster_id"
 }
 
-# Function to update the advertised listeners by modifying BROKER:// and CC:// listeners adding the hostname prefix
+# Function to update the advertised listeners by modifying BROKER:// listeners adding the hostname prefix
 update_advertised_listeners() {
   # Use tr to replace commas with newlines and read into an array
   readarray -t elements < <(echo "$advertised_listeners" | tr ',' '\n')
-
   # Prefix to append to each element
   prefix=$HOSTNAME
-
   # Loop through the array and modify elements
   modified_elements=()
   for element in "${elements[@]}"; do
@@ -73,32 +84,29 @@ update_advertised_listeners() {
           modified_elements+=("$element")  # Skip modification
         fi
   done
-
   # Join the modified elements into a string using commas as delimiters
   output_string=$(IFS=','; echo "${modified_elements[*]}")
   advertised_listeners=$output_string
-
   # Print the modified string
   echo "Modified advertised_listeners: $advertised_listeners"
 }
-# This script copies the temporary operator configuration file to the operator configuration file 
-# and merges the custom configuration files based on the process roles specified in the operator configuration file. 
-# The merged configuration file is saved in the Kafka configuration directory and move the file to operator configuration.
+# This script copies the temporary operator configuration file to the operator configuration file
 cp $temp_operator_config $operator_config
-roles=$(grep process.roles $operator_config | cut -d'=' -f 2-)
-if [[ $roles = "controller" ]]; then
-  /opt/kafka/config/merge_custom_config.sh $controller_config_file $operator_config $kafkaconfig_dir/config.properties.merged
-elif [[ $roles = "broker" ]]; then
-  /opt/kafka/config/merge_custom_config.sh $broker_config_file $operator_config $kafkaconfig_dir/config.properties.merged
-else [[ $roles = "controller,broker" ]]
-  /opt/kafka/config/merge_custom_config.sh $server_config_file $operator_config $kafkaconfig_dir/config.properties.merged
-fi
-
 # If a temporary SSL configuration file exists, it concatenates the contents of the temporary SSL configuration file to operator configuration file.
 if [[ -f $temp_ssl_config ]]; then
   cat $temp_ssl_config $operator_config > config.properties.updated
   mv config.properties.updated $operator_config
   cp $temp_ssl_config /opt/kafka/config
+fi
+# and merges the custom configuration files based on the process roles specified in the operator configuration file.
+# The merged configuration file is saved in the Kafka configuration directory and move the file to operator configuration.
+roles=$(grep process.roles $operator_config | cut -d'=' -f 2-)
+if [[ $roles = "controller" ]]; then
+  /opt/kafka/config/merge_custom_config.sh $controller_custom_config $operator_config $kafka_config_dir/config.properties.merged
+elif [[ $roles = "broker" ]]; then
+  /opt/kafka/config/merge_custom_config.sh $broker_custom_config $operator_config $kafka_config_dir/config.properties.merged
+else [[ $roles = "controller,broker" ]]
+  /opt/kafka/config/merge_custom_config.sh $server_custom_config $operator_config $kafka_config_dir/config.properties.merged
 fi
 
 # If a file named $temp_clientauth_config exists, it copies the file to /opt/kafka/config directory.
@@ -113,7 +121,7 @@ if [[ $KAFKA_PASSWORD != "" ]]; then
   sed -i "s/\<KAFKA_USER\>/"$KAFKA_USER"/g" $CLIENTAUTHFILE
   sed -i "s/\<KAFKA_PASSWORD\>/"$KAFKA_PASSWORD"/g" $CLIENTAUTHFILE
   
-  sed -i "s/\<KAFKA_USER\>/"$KAFKA_USER"/g" $operator_config
+  sed -i "s/KAFKA_USER\>/"$KAFKA_USER"/g" $operator_config
   sed -i "s/\<KAFKA_PASSWORD\>/"$KAFKA_PASSWORD"/g" $operator_config
 fi
 
@@ -122,19 +130,13 @@ fi
 # The script replaces dots (.) in the keys with underscores (_) to make them valid environment variable names.
 while IFS='=' read -r key value
 do
-    key=$(echo $key | tr '.' '_')
+    key=$(echo "$key" | sed -e 's/\./_/g' -e 's/-/___/g')
     eval ${key}=\${value}
 done < "$operator_config"
 
-# Set the value of CLUSTER_ID, ID and NODE environment variables.
-CLUSTER_ID=${cluster_id}
+# Set the value of KAFKA_CLUSTER_ID and ID
+export KAFKA_CLUSTER_ID=${KAFKA_CLUSTER_ID:-$cluster_id}
 ID=${HOSTNAME##*-}
-NODE=$(echo $HOSTNAME | rev | cut -d- -f1 --complement | rev )
-
-AUTHFILE="/opt/kafka/config/kafka_server_jaas.conf"
-sed -i "s/\<KAFKA_USER\>/"$KAFKA_USER"/g" $AUTHFILE
-sed -i "s/\<KAFKA_PASSWORD\>/"$KAFKA_PASSWORD"/g" $AUTHFILE
-export KAFKA_OPTS="$KAFKA_OPTS -Djava.security.auth.login.config=$AUTHFILE"
 
 if [[ -n $advertised_listeners ]]; then
   old_advertised_listeners=$advertised_listeners
@@ -148,8 +150,8 @@ fi
 # Removes comments and empty lines from a file.
 # Arguments -> A properties file
 function remove_comments_and_sort() {
-  sed -i '/^#/d;/^$/d' $1
-  sort -o $1 $1
+  sed -i '/^#/d;/^$/d' "$1"
+  sort -o "$1" "$1"
 }
 
 # It starts the Kafka server with the specified configuration.
@@ -157,77 +159,80 @@ function remove_comments_and_sort() {
 # it deletes the cluster metadata,
 # sets the node ID, updates the log directories,
 # and formats the storage using kafka-storage script before starting the Kafka server.
-if [[ $process_roles = "controller" ]]; then
+old_log_dirs="$log_dirs"
+if [[ "$process_roles" = "controller" ]]; then
   ID=$(( ID + kafka_broker_max_id ))
   delete_cluster_metadata $ID
-
-  echo "node.id=$ID" >> /opt/kafka/config/kafkaconfig/config.properties
-
-  sed -e "s+^log.dirs=.*+log.dirs=$log_dirs/$ID+" \
-  /opt/kafka/config/kafkaconfig/config.properties > config.properties.updated
-  mv config.properties.updated /opt/kafka/config/kafkaconfig/config.properties
-
-  cat /opt/kafka/config/kafkaconfig/config.properties /opt/kafka/config/kraft/controller.properties | awk -F= '!seen[$1]++' > controller.properties.updated
-  mv controller.properties.updated /opt/kafka/config/kraft/controller.properties
-
-  if [[ -f "$ssl_config" ]]; then
-      cat $ssl_config $controller_config | awk -F'=' '!seen[$1]++' > controller.properties.updated
-      mv controller.properties.updated $controller_config
-  fi
-
-  remove_comments_and_sort $controller_config
-
-  echo "Formatting controller properties"
-  kafka-storage.sh format -t "$CLUSTER_ID" -c /opt/kafka/config/kraft/controller.properties --ignore-formatted
-  echo "Starting Kafka Server"
-  exec kafka-server-start.sh /opt/kafka/config/kraft/controller.properties
-
-elif [[ $process_roles = "broker" ]]; then
-
+  echo "node.id=$ID" >> "$operator_config"
+  sed -i "s|"^log.dirs=$old_log_dirs"|"log.dirs=$log_dirs"|" "$operator_config"
+  cat $operator_config $controller_config | awk -F= '!seen[$1]++' > "$controller_config.updated"
+  mv "$controller_config.updated" "$final_config"
+elif [[ "$process_roles" = "broker" ]]; then
   delete_cluster_metadata $ID
-
-  echo "node.id=$ID" >> $operator_config
-
-  sed -e "s+^log.dirs=.*+log.dirs=$log_dirs/$ID+" \
-  $operator_config > $operator_config.updated
-  mv $operator_config.updated $operator_config
-
-  cat $operator_config $broker_config | awk -F'=' '!seen[$1]++' > $broker_config.updated
-  mv $broker_config.updated $broker_config
-
-  if [[ -f "$ssl_config" ]]; then
-      cat $ssl_config $broker_config | awk -F'=' '!seen[$1]++' > $broker_config.updated
-      mv $broker_config.updated $broker_config
-  fi
-
-  remove_comments_and_sort $broker_config
-
-  echo "Formatting broker properties"
-  kafka-storage.sh format -t $CLUSTER_ID -c $broker_config --ignore-formatted
-  echo "Starting Kafka Server"
-  exec kafka-server-start.sh $broker_config
-
-else [[ $process_roles = "controller,broker" ]]
-
-  delete_cluster_metadata $ID
-
-  echo "node.id=$ID" >> $operator_config
-  sed -e "s+^log.dirs=.*+log.dirs=$log_dirs/$ID+" \
-  $operator_config > $operator_config.updated
-  mv $operator_config.updated $operator_config
-
-  cat $operator_config $server_config | awk -F'=' '!seen[$1]++' > $server_config.updated
-  mv $server_config.updated $server_config
-
-  if [[ -f "$ssl_config" ]]; then
-      cat $ssl_config $server_config | awk -F'=' '!seen[$1]++' > $server_config.updated
-      mv $server_config.updated $server_config
-  fi
-
-  remove_comments_and_sort $server_config
-
-  echo "Formatting server properties"
-  kafka-storage.sh format -t $CLUSTER_ID -c $server_config --ignore-formatted
-  echo "Starting Kafka Server"
-  exec kafka-server-start.sh $server_config
+  echo "node.id=$ID" >> "$operator_config"
+  sed -i "s|"^log.dirs=$old_log_dirs"|"log.dirs=$log_dirs"|" "$operator_config"
+  cat "$operator_config" "$broker_config" | awk -F'=' '!seen[$1]++' > "$broker_config.updated"
+  mv "$broker_config.updated" "$final_config"
+else [[ "$process_roles" = "controller,broker" ]]
+  delete_cluster_metadata "$ID"
+  echo "node.id=$ID" >> "$operator_config"
+  sed -i "s|"^log.dirs=$old_log_dirs"|"log.dirs=$log_dirs"|" "$operator_config"
+  cat "$operator_config" "$server_config" | awk -F'=' '!seen[$1]++' > "$server_config.updated"
+  mv "$server_config.updated" "$final_config"
 fi
+
+remove_comments_and_sort "$final_config"
+
+# Keeping this for backward compatibility
+if grep -Eqi '^sasl\.enabled\.mechanisms=.*plain.*' "$final_config"; then
+  AUTHFILE="/opt/kafka/config/kafka_server_jaas.conf"
+  sed -i "s/KAFKA_USER\>/"$KAFKA_USER"/g" $AUTHFILE
+  sed -i "s/\<KAFKA_PASSWORD\>/"$KAFKA_PASSWORD"/g" $AUTHFILE
+  export KAFKA_OPTS="$KAFKA_OPTS -Djava.security.auth.login.config=$AUTHFILE"
+fi
+# If user has provided custom log4j configuration, it will be used
+if [[ -f "$custom_log4j_config" ]]; then
+  cp "$custom_log4j_config" /opt/kafka/config
+fi
+# If user has provided custom tools-log4j configuration, it will be used
+if [[ -f "$custom_tools_log4j_config" ]]; then
+  cp "$custom_tools_log4j_config" /opt/kafka/config
+fi
+
+addKafkactlConfig() {
+  if [[ "$process_roles" == "controller" ]]; then
+    return
+  fi
+  kafkactl_config_path="/opt/kafka/.config/kafkactl/config.yml"
+  mkdir -p "$(dirname "$kafkactl_config_path")"
+  cat <<EOL > "$kafkactl_config_path"
+contexts:
+  default:
+    brokers:
+      - "localhost:9092"
+EOL
+  CLIENTAUTHFILE="/opt/kafka/config/clientauth.properties"
+  if [[ -f "$CLIENTAUTHFILE" ]]; then
+    cat <<EOL >> "$kafkactl_config_path"
+    sasl:
+      enabled: true
+      mechanism: plaintext
+      username: "$KAFKA_USER"
+      password: "$KAFKA_PASSWORD"
+EOL
+  fi
+  if grep -Ei "^security\.protocol=sasl_ssl" "$CLIENTAUTHFILE"; then
+    cat <<EOL >> "$kafkactl_config_path"
+    tls:
+      enabled: true
+      ca: "/var/private/ssl/ca.crt"
+      cert: "/var/private/ssl/tls.crt"
+      certKey: "/var/private/ssl/tls.key"
+      insecure: false
+EOL
+  fi
+  echo "current-context: default" >> "$kafkactl_config_path"
+}
+
+addKafkactlConfig
+/opt/kafka/config/start.sh "$final_config"
